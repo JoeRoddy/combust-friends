b8c4a74175836109170a3bef086a4a03783065cc
Joe <mrjoeroddy@gmail.com>
2018-03-21 11:27:59 -0400

    remove unused functions

--- stores/FriendStore.js
+++ stores/FriendStore.js
@@ -42,17 +42,8 @@ class FriendStore {
     this.usersLoaded = true;
   }
 
-  storeFriend(friendId, friend) {
-    userStore.saveUserLocally(friendId, friend);
-    this.friendIdsMap.set(friendId, true);
-  }
-
   isFriend(userId) {
     return this.activeFriendshipsByFriendIdMap.has(userId);
-  }
-
-  getFriend(userId) {
-    return this.isFriend(userId) ? userStore.getUserById(userId) : null;
   }
 
   /**
