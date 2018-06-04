e18e1f8c71a753d542585a3f5672aea18a0dd9ac
Joe Roddy <mrjoeroddy@gmail.com>
2018-06-04 09:54:55 -0400

    mobile component

---
+++ mobile_components/FriendsList.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import { observer } from "mobx-react";
+import { Text } from "react-native";
+
+import nav from "../../helpers/NavigatorHelper";
+import friendStore from "../../stores/FriendStore";
+import { Button, Screen } from "../reusable";
+import UserList from "../users/UserList";
+
+const FriendsList = observer(() => {
+  const { friends } = friendStore;
+  const isFriendless = friends !== {};
+
+  return (
+    <Screen title="Friends" noPadding={!isFriendless}>
+      {isFriendless ? (
+        <FindFriends />
+      ) : (
+        <UserList
+          users={friendStore.friends}
+          onUserClicked={user => nav.navigate("Profile", { id: user.id })}
+        />
+      )}
+    </Screen>
+  );
+});
+
+export default FriendsList;
+
+const FindFriends = () => (
+  <Fragment>
+    <Text style={{ margin: 10 }}>You have no friends</Text>
+    <Button title="Find Friends" onPress={() => nav.navigate("UserSearch")} />
+  </Fragment>
+);
